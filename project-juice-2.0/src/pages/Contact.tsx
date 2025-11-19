56485e2463113d69e538713f5aaf4cebfd0769a0
highimexy <teslawiktor@gmail.com>
2025-11-19 22:27:51 +0100

    H1 + p na contact page

--- project-juice-2.0/src/pages/Contact.tsx
+++ project-juice-2.0/src/pages/Contact.tsx
@@ -1,27 +1,44 @@
+import Navigation from "../components/Navigation.tsx";
 import { BasicTile } from "../components/BasicTile/BasicTile.tsx";
 
 function Contact() {
   return (
     <>
-      <div className="basic-tile-main">
-        
-        <BasicTile>
-          <div className="contact-formulage">
-            <div className="email-input">
-              <p>Email:</p>
-              <input className="input" placeholder="Email" type=""></input>
-            </div>
-            <div className="email-input">
-              <p>Imie:</p>
-              <input className="input" placeholder="Imie" type=""></input>
-            </div>
-            <div className="email-input">
-              <p>Zamowienie:</p>
-              <input className="input" placeholder="Zamowienie" type=""></input>
-            </div>
-            <button>Wyslij Wiadomosc</button>
+      <div className="content-wrapper">
+        <div className="contact">
+          <div className="">
+            <Navigation />
           </div>
-        </BasicTile>
+          <div className="contact-hero">
+            <h1 className="contact-title">Skontaktuj sie z juiice.pl</h1>
+            <p className="taste-hero-1 pnf-h1 ">Wypelnij formularz </p>
+            <p className="taste-hero-2 pnf-h1">Szybka odpowiedz</p>
+            <p className="taste-hero-3 pnf-h1 taste-hero-last">
+              Niezapomniany smak
+            </p>
+          </div>
+          <BasicTile>
+            <div className="contact-formulage">
+              <div className="email-input">
+                <p>Email:</p>
+                <input className="input" placeholder="Email" type=""></input>
+              </div>
+              <div className="email-input">
+                <p>Imie:</p>
+                <input className="input" placeholder="Imie" type=""></input>
+              </div>
+              <div className="email-input">
+                <p>Zamowienie:</p>
+                <input
+                  className="input"
+                  placeholder="Zamowienie"
+                  type=""
+                ></input>
+              </div>
+              <button>Wyslij Wiadomosc</button>
+            </div>
+          </BasicTile>
+        </div>
       </div>
     </>
   );
