489e4b4c790cfc3e5a78abf6d6bc231432b86c29
highimexy <teslawiktor@gmail.com>
2025-11-28 19:53:05 +0100

    port change

--- project-juice-2.0-backend/server.js
+++ project-juice-2.0-backend/server.js
@@ -6,7 +6,7 @@ import cors from 'cors';
 dotenv.config(); 
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors()); 
 app.use(express.json()); 
