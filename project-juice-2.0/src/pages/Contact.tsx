6c0b895109e1bc1957f299d268ea00e9e1c5726b
highimexy <teslawiktor@gmail.com>
2025-11-28 20:23:42 +0100

    fix

--- project-juice-2.0/src/pages/Contact.tsx
+++ project-juice-2.0/src/pages/Contact.tsx
@@ -33,7 +33,7 @@ function Contact() {
     }
 
     try {
-      const response = await fetch("https://api.juiice.pl/", {
+      const response = await fetch("https://api.juiice.pl", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
